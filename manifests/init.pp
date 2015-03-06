f833513506e988176fa12f7a4ffefa3918617226
Adam <adam@statystyka.net>
2015-03-06 15:55:46 +0100

    x

--- manifests/init.pp
+++ manifests/init.pp
@@ -216,10 +216,10 @@ define lxc (
         local_user  => $user,
         remote_user => "root@${::fqdn}",
         #        remote_user => "root@$localhost",
-        require     => Sshkeys::Create_ssh_key[$user]
+        require     => [Sshkeys::Create_ssh_key[$user], File["${user_home}/.ssh/id_rsa"]]
       }
 
-      sshkeys::create_ssh_key { $user: }
+      #      sshkeys::create_ssh_key { $user: }
 
       #      augeas { "lxc ${name} lxc.network.link":
       #        incl    => "${config_file}",
